09d1f8a385debb12f558db17dbb7d1b927680686
aejmcclelland <aejmcclelland@googlemail.com>
2025-04-19 18:58:31 +0100

    removed unecesary files

--- src/app/(payload)/api/route.ts
+++
@@ -1,7 +0,0 @@
-// src/app/api/route.ts
-import { nextjsRouteHandler } from '@payloadcms/next/route-handler';
-import config from '@payload-config';
-
-export const { GET, POST, PUT, DELETE } = nextjsRouteHandler({
-	config,
-});
