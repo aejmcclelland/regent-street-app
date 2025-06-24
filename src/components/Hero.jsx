d94ac3fbd5ec2acdba2b4e9060afec016872722f
aejmcclelland <aejmcclelland@googlemail.com>
2025-06-24 22:14:09 +0100

    fixed carousel height on mobile devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ export default function Hero({ images }) {
             interval={5000} // 5 seconds per slide
         >
             {images.map((img) => (
-                <div key={img.src} className="relative">
+                <div key={img.src} className="relative aspect-[4/3] sm:aspect-[16/6] md:aspect-[16/5] lg:aspect-[16/4] xl:aspect-[16/4]">
                     <CldImage
                         src={img.src}
                         width={1920}
@@ -26,7 +26,7 @@ export default function Hero({ images }) {
                         alt={img.alt}
                         crop="fill"
                         gravity="auto"
-                        className="w-auto h-auto shadow-lg" // Fix aspect ratio
+                        className="w-full h-full object-cover shadow-lg" // Fix aspect ratio
                     />
                     <p className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-50 text-white px-4 py-2 rounded-md">
                         {img.caption}
