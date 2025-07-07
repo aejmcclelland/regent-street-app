50ce5bfa59676a6cdf897b1a67ae2e2db143f13f
aejmcclelland <aejmcclelland@googlemail.com>
2025-07-07 09:06:31 +0100

    added safeguarding page and improved footer styling

---
+++ src/app/(my-app)/safeguarding/page.tsx
@@ -0,0 +1,10 @@
+import { FaPhone } from 'react-icons/fa6';
+import { MdOutlineEmail, MdPlace } from 'react-icons/md';
+
+export default async function Safeguarding() {
+	return (
+		<div className='w-full'>
+			<div className='bg-base-200 py-10 px-6 md:px-12 text-base-content'></div>
+		</div>
+	);
+}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
 
                 {/* Church Info */}
                 <div className="flex flex-col items-center text-center lg:items-start lg:text-left w-full">
-                    <h6 className="footer-title text-primary">Regent Street Presbyterian</h6>
+                    <h6 className="footer-title text-warning text-lg">Regent Street Presbyterian</h6>
                     <p className="text-sm text-gray-400">
                         &copy; {currentYear} Regent Street Presbyterian Church. All rights reserved.
                     </p>
@@ -18,7 +18,7 @@ export default function Footer() {
 
                 {/* Contact Info */}
                 <div className="flex flex-col items-center text-center lg:items-start lg:text-left w-full">
-                    <h6 className="footer-title text-primary">Contact Us</h6>
+                    <h6 className="footer-title text-warning text-lg">Contact Us</h6>
                     <p className="text-sm text-gray-400">48a Regent Street, Newtownards, BT23 4LP</p>
                     <a href="mailto:regent.st@btconnect.com" className="text-primary hover:underline text-sm">
                         regent.st@btconnect.com
@@ -28,13 +28,13 @@ export default function Footer() {
 
                 {/* Info */}
                 <div className="flex flex-col items-center text-center lg:items-start lg:text-left w-full">
-                    <h6 className="footer-title text-primary">Info</h6>
-                    <p className="text-sm text-gray-400">
-                        Registered with the Charity Commission for Northern Ireland NIC105068.
-                    </p>
-                    <p className="text-sm text-gray-400 mt-2">
-                        Regent Street Presbyterian Church is not responsible for the content of external websites nor does it necessarily endorse their sponsoring organizations.
-                    </p>
+                    <h6 className="footer-title text-warning text-lg">Info</h6>
+                    <a
+                        href="/who-we-are/safeguarding"
+                        className="text-primary hover:underline text-sm block"
+                    >
+                        Safeguarding Policy
+                    </a>
                     <a
                         href="https://www.presbyterianireland.org/getmedia/5ca4e3fe-f368-4d15-b8a7-779e2f3c843a/PCI_Internet_Guidelines.pdf.aspx"
                         target="_blank"
@@ -42,13 +42,13 @@ export default function Footer() {
                         className="text-primary hover:underline text-sm block mt-2"
                     >
                         PCI Internet Use Guidelines
-                        
+
                     </a>
                 </div>
 
                 {/* Social Media */}
                 <div className="flex flex-col items-center text-center lg:items-start lg:text-left w-full">
-                    <h6 className="footer-title text-primary">Connect with Us</h6>
+                    <h6 className="footer-title text-warning text-lg">Connect with Us</h6>
                     <div className="flex space-x-4 mt-2">
                         <a
                             href="https://www.facebook.com/yourpage"
@@ -68,6 +68,15 @@ export default function Footer() {
                         </a>
                     </div>
                 </div>
+
+            </div>
+            <div className="mt-8 px-6 text-center text-gray-400 space-y-1 text-sm">
+                <p>
+                    Regent Street Presbyterian Church is not responsible for the content of external websites nor does it necessarily endorse their sponsoring organizations.
+                </p>
+                <p>
+                    Registered with the Charity Commission for Northern Ireland NIC105068.
+                </p>
             </div>
         </footer>
     );
