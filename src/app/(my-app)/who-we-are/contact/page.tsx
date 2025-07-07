a92a18b4a7f11ef52f5366082a2d8bb71218ca3c
aejmcclelland <aejmcclelland@googlemail.com>
2025-07-07 08:23:53 +0100

    added place icon to contact page

--- src/app/(my-app)/who-we-are/contact/page.tsx
+++ src/app/(my-app)/who-we-are/contact/page.tsx
@@ -1,6 +1,6 @@
 import Banner from '@components/Banner';
 import { FaPhone } from 'react-icons/fa6';
-import { MdOutlineEmail } from 'react-icons/md';
+import { MdOutlineEmail, MdPlace } from 'react-icons/md';
 import { getCollectionContent } from 'lib/payload';
 import FindUs from '@components/FindUs';
 
@@ -20,8 +20,13 @@ export default async function ContactUs() {
 					<div className='space-y-2 bg-secondary text-secondary-content p-6 rounded-md'>
 						<h2 className='text-3xl font-bold'>Address</h2>
 						<div className='bg-red-50 rounded-md p-4 text-lg'>
-							<p className='text-xl'>48a Regent St, Newtownards BT23 4LP</p>
-							<div className='flex items-center gap-2'>
+							<div className='flex items-center gap-2 mb-2.5'>
+								<MdPlace className='text-2xl text-primary' />
+								<p className='text-primary text-xl'>
+									48a Regent St, Newtownards BT23 4LP
+								</p>
+							</div>
+							<div className='flex items-center gap-2 mb-2.5'>
 								<FaPhone className='text-2xl text-primary' />
 								<a
 									href='tel:02891822416'
@@ -29,7 +34,7 @@ export default async function ContactUs() {
 									028 9182 2416
 								</a>
 							</div>
-							<div className='flex items-center gap-2'>
+							<div className='flex items-center gap-2 mb-2'>
 								<MdOutlineEmail className='text-2xl text-primary' />
 								<a
 									href='mailto:regent.st@btconnect.com'
